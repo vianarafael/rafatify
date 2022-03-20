53e4dda5e23e26f9343ef3ce0aa7c5f7ac79e9ae
Rafael Viana <vianarafaelds@gmail.com>
2022-03-20 20:12:04 +0900

    sidebar wip

---
+++ components/sidebar.tsx
@@ -0,0 +1,35 @@
+import NextImage from "next/image";
+import {
+  Box,
+  List,
+  ListIcon,
+  ListItem,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+export default function Sidebar() {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingY="20px">
+          <NextImage src="/barely_functional.png" height={60} width={120} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
