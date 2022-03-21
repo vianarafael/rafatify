bd0fe6072d355aa3c5e47181f685a26c271f77d1
Rafael Viana <vianarafaelds@gmail.com>
2022-03-21 12:21:33 +0900

    playlists display

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -49,6 +49,8 @@ const musicMenu = [
   },
 ];
 
+const playlists = new Array(30).fill(1).map((el, i) => `Playlist ${i + 1}`);
+
 export default function Sidebar() {
   return (
     <Box
@@ -58,7 +60,7 @@ export default function Sidebar() {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="96%">
         <Box width="120px" marginBottom="20px" paddingY="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
@@ -82,7 +84,7 @@ export default function Sidebar() {
             ))}
           </List>
         </Box>
-        <Divider color="gray.800" />
+
         <Box marginTop="20px">
           <List spacing={2}>
             {musicMenu.map((item) => (
@@ -97,6 +99,20 @@ export default function Sidebar() {
                       />
                       {item.name}
                     </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" />
+        <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
