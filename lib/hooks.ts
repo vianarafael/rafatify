6fd034758c3c5c8c94ee89cd883e9bc4a5f8e059
Rafael Viana <vianarafaelds@gmail.com>
2022-04-01 13:08:51 +0900

    custom hook for swr

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -11,3 +11,13 @@ export const useMe = () => {
     isError: error,
   };
 };
+
+export const usePlaylist = () => {
+  const { data, error } = useSWR("/playlist", fetcher);
+
+  return {
+    playlists: data || [],
+    isLoading: !data && !error,
+    isError: error,
+  };
+};
